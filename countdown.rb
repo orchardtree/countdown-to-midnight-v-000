cb93d403e11b7fe3cbf4c66b9eef4bd376108341
Jesse Wenger <wengerj@gmail.com>
2021-03-03 00:22:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,10 @@ def countdown(time)
 end
 
 def countdown_with_sleep(time)
-  timer = 0
-  while timer <= 5
-    timer += 1
-    while time > 0
-      time -= 1
-    end
+  new_year = "HAPPY NEW YEAR!"
+  while time > 0 
+    puts "#{time} SECOND(S)!"
+    time -= 1
   end
+  new_year
 end
