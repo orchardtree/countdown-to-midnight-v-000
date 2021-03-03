cb2c235edfea7835247497803cc53c45d3e91f25
Jesse Wenger <wengerj@gmail.com>
2021-03-03 01:03:19 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+require 'pry'
+def countdown(time)
+  new_year = "HAPPY NEW YEAR!"
+  while time > 0 
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  new_year
+end
 
-def countdown
+def countdown_with_sleep(time)
+  sleep(5)
+  while time > 0
+    time -= 1
+  end
 end
