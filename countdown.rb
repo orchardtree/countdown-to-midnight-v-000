de3e4ff4f3a85c8003e8741327be3eae25a9f946
Jesse Wenger <wengerj@gmail.com>
2021-03-03 00:57:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,9 +13,10 @@ def countdown_with_sleep(time)
   timer = 0
   while timer < 5
     timer += 1
-    binder.pry
+    binding.pry
   end
   while time > 0
     time -= 1
+    binding.pry
   end
 end
